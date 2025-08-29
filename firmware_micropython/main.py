8aa9aaf36745c62b8682004fb7e16f9bef2278ba
Jana <janamarie@janamarie.dev>
2025-08-29 17:00:18 +0200

    moved handler

--- firmware_micropython/main.py
+++ firmware_micropython/main.py
@@ -23,61 +23,6 @@ I2C0_SDA = Pin(5)
 I2C0_STUSB4500_ADDR = 0x28
 I2C0_DISPLAY_ADDR = 0x3C
 
-class handler(object):
-    """docstring for handler"""
-    def __init__(self, led_ok, led_act, trig):
-        super(handler, self).__init__()
-        self.lok = led_ok
-        self.lac = led_act
-
-        self.stage = macroStage(trig, led_ok, led_act)
-
-        self.lp2 = lp("Handler")
-        self.lp2.print("init done")
-
-    def handle(self, cs):
-        self.lac.value(1)
-        try:
-            req = cs.read()
-            if req:
-                self.lp2.print("serving request " + str(req))
-                clist = [line for line in req.decode("utf-8").split('\n') if "Referer" in line][0][:-1].replace('?', '&').split('&')
-                if len(clist > 1):
-                    self.lp2.print("parsing config")
-                    cdict = {}
-                    for s in clist:
-                        c,a = s.split('=')
-                        cdict[c] = a
-                    self.lp2.print(cdict)
-                    if cdict["cmd"] == "stop":
-                        stage.stop()
-                    elif cdict["cmd"] == "home":
-                        stage.home()
-                    elif cdict["cmd"] == "run":
-                        # start, steps, dps, delay, spd, req
-                        start = int(cdict['start'])
-                        steps = int(cdict['steps'])
-                        dps = int(cdict['dps'])
-                        delay = int(cdict['delay'])
-                        speed = int(cdict['speed'])
-                        req = cdict['req']
-                        stage.run(start, steps, dps, delay, spd, req)
-                    elif cdict["cmd"] == "moveTo":
-                        stage.moveTo(int(cdict['pos']))
-                    elif cdict["cmd"] == "moveRel":
-                        stage.moveRel(int(cdict['pos']))
-                    else:
-                        self.lp2.print("no command found")
-
-                rep = b'Hello\r\n'
-                cs.write(rep)
-            else:
-                self.lp2.print("connection closed")
-        except Exception as e:
-            self.lp2.print("ERROR " + str(e))
-        cs.close()
-        self.lac.value(0)
-
 class webcom(object):
     """docstring for webcom"""
     def __init__(self, ssid, password, led_ok, led_act, trig):
@@ -87,10 +32,21 @@ class webcom(object):
         self.lok = led_ok
         self.lac = led_act
 
-        self.handler = handler(led_ok, led_act, trig)
+        #self.handler = handler(led_ok, led_act, trig)
+
+        self.index = open('index.html')
+        self.reply = self.index.read()
+
+        self.stage = macroStage(trig, led_ok, led_act)
 
         self.lp1 = lp("WebCom")
         self.lp1.print("init done")
+
+        self.lp2 = lp("Handler")
+        self.lp2.print("init done")
+        
+        self.lp3 = lp("Handler Command")
+        self.lp3.print("init done")
     
     def connect(self):
         self.lp1.print("connecting to " + self.ssid)
@@ -118,30 +74,64 @@ class webcom(object):
         self.srv.setblocking(False)
         self.srv.setsockopt(socket.SOL_SOCKET, 20, self.cln_handler)
         self.lp1.print("server running on port 80")
-    '''
+    
     def req_handler(self, cs):
         self.lac.value(1)
+        req = b''
         try:
             req = cs.read()
             if req:
-                self.lp1.print("serving request " + str(req))
-                rep = b'Hello\r\n'
-                cs.write(rep)
+                cs.write(self.reply)
             else:
-                self.lp1.print("connection closed")
-                pass
+                self.lp2.print("connection closed")
         except Exception as e:
-            self.lp1.print("ERROR " + str(e))
-            pass
+            self.lp2.print("ERROR " + str(e))
         cs.close()
+
+        if 'GET /?' in req.decode("utf-8"):
+            #clist = [line for line in req.decode("utf-8").split('\n') if 'GET /?' in line][0][:-1].replace('?', '&').split('&')
+            clist = [line for line in req.decode("utf-8").split('\n') if 'GET /?' in line][0].split(' ')[1][2:].split('&')
+
+            if len(clist) > 0:
+                self.lp2.print("parsing config")
+                cdict = {}
+                for s in clist:
+                    c,a = s.split('=')
+                    cdict[c] = a
+                if cdict["cmd"] == "stop":
+                    self.lp3.print("stop")
+                    self.stage.stop()
+                elif cdict["cmd"] == "home":
+                    self.lp3.print("home")
+                    self.stage.home()
+                elif cdict["cmd"] == "run":
+                    self.lp3.print("run macro stage with parameters " + cdict['start'] + " " + cdict['steps'] + " " + cdict['dps'] + " " + cdict['delay'] + " " + cdict['speed'])
+                    self.lp3.print("send camera request to " + cdict['req'])
+                    # start, steps, dps, delay, spd, req
+                    start = int(cdict['start'])
+                    steps = int(cdict['steps'])
+                    dps = int(cdict['dps'])
+                    delay = int(cdict['delay'])
+                    speed = int(cdict['speed'])
+                    req = cdict['req']
+                    self.stage.run(start, steps, dps, delay, spd, req)
+                elif cdict["cmd"] == "moveTo":
+                    self.lp3.print("move to position ", cdict['pos'])
+                    self.stage.moveTo(int(cdict['pos']))
+                elif cdict["cmd"] == "moveRel":
+                    self.lp3.print("move relative by ", cdict['pos'])
+                    self.stage.moveRel(int(cdict['pos']))
+                else:
+                    self.lp3.print("no command found")
+
         self.lac.value(0)
-    '''
+    
     def cln_handler(self, srv):
         self.lac.value(1)
         cs,ca = self.srv.accept()
         self.lp1.print("serving " + str(ca))
         cs.setblocking(False)
-        cs.setsockopt(socket.SOL_SOCKET, 20, self.handler.handle) #self.handler.handle(self.handler, cs))
+        cs.setsockopt(socket.SOL_SOCKET, 20, self.req_handler(cs)) #self.handler.handle(self.handler, cs))
         self.lac.value(0)
 
 class lp(object):
